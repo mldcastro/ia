421a2cda7acf737548614f5c60107bcb42bfc044
Clara Theisen <kiwib1r9@gmail.com>
2025-09-15 19:57:59 -0300

    feat: implement fit

--- src/aprendizado_supervisionado/alegrete.py
+++ src/aprendizado_supervisionado/alegrete.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import numpy.typing as npt
 
 def compute_mse(b, w, data):
     """
@@ -24,7 +24,13 @@ def step_gradient(b, w, data, alpha):
     raise NotImplementedError  # substituir pelo seu codigo
 
 
-def fit(data, b, w, alpha, num_iterations):
+def fit(
+    data: npt.NDArray[np.floating], 
+    b: float, 
+    w: float, 
+    alpha: float, 
+    num_iterations: int
+) -> tuple[list[float], list[float]]:
     """
     Para cada época/iteração, executa uma atualização por descida de
     gradiente e registra os valores atualizados de b e w.
@@ -39,4 +45,16 @@ def fit(data, b, w, alpha, num_iterations):
     :param num_iterations: int - numero de épocas/iterações para executar a descida de gradiente
     :return: list,list - uma lista com os b e outra com os w obtidos ao longo da execução
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    # inicializa b_history e w_history vazios
+    b_history: list[float] = []
+    w_history: list[float] = []
+    # para cada época
+    for _ in range(num_iterations):
+        # desce o gradiente
+        b, w = step_gradient(b, w, data, alpha)
+        # append b em b_history
+        # append w em w_history
+        b_history.append(b)
+        w_history.append(w)
+
+    return b_history, w_history
