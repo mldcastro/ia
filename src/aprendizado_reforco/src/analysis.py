da093571aa01fe9db1f0ed4e37497aae4667d933
Matheus Luís de Castro <matheuslc36.mc@gmail.com>
2025-10-13 22:31:51 -0300

    Trabalho 2 - questão 3

--- src/aprendizado_reforco/src/analysis.py
+++ src/aprendizado_reforco/src/analysis.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -19,14 +19,52 @@
 # Set the given parameters to obtain the specified policies through
 # value iteration.
 
+
 def question2():
     answerDiscount = 0.9
     answerNoise = 0.2
     return answerDiscount, answerNoise
 
-if __name__ == '__main__':
-    print('Answers to analysis questions:')
+
+def question3a():
+    answerDiscount = 0.3
+    answerNoise = 0.8
+    reward = 0.0
+    return answerDiscount, answerNoise, reward
+
+
+def question3b():
+    answerDiscount = 0.9
+    answerNoise = 0.0
+    reward = 0.0
+    return answerDiscount, answerNoise, reward
+
+
+def question3c():
+    answerDiscount = 0.9
+    answerNoise = 0.0
+    reward = 0.0
+    return answerDiscount, answerNoise, reward
+
+
+def question3d():
+    answerDiscount = 0.9
+    answerNoise = 0.0
+    reward = 0.0
+    return answerDiscount, answerNoise, reward
+
+
+def question3e():
+    answerDiscount = 0.9
+    answerNoise = 0.0
+    reward = 0.0
+    return answerDiscount, answerNoise, reward
+
+
+if __name__ == "__main__":
+    print("Answers to analysis questions:")
     import analysis
-    for q in [q for q in dir(analysis) if q.startswith('question')]:
+
+    for q in [q for q in dir(analysis) if q.startswith("question")]:
         response = getattr(analysis, q)()
-        print('  Question %s:\t%s' % (q, str(response)))
+        print("  Question %s:\t%s" % (q, str(response)))
