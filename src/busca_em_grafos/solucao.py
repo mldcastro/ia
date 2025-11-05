6cd6764291a0579f2e0af1ae9eb0160fd1516eb4
Matheus Luís de Castro <matheuslc36.mc@gmail.com>
2025-11-04 21:30:20 -0300

    Trabalho 3 - sucessor

--- src/busca_em_grafos/solucao.py
+++ src/busca_em_grafos/solucao.py
@@ -1,4 +1,71 @@
-from typing import Self
+import enum
+import typing
+
+
+class Action(enum.StrEnum):
+    UP = "acima"
+    DOWN = "abaixo"
+    RIGHT = "direita"
+    LEFT = "esquerda"
+
+
+class PuzzleState:
+    N_ROWS: typing.Final[int] = 3
+    N_COLS: typing.Final[int] = 3
+    N_POSITIONS: typing.Final[int] = 9
+
+    OBJECTIVE_STATE: typing.Final[str] = "12345678_"
+    BLANK_CHAR: typing.Final[str] = "_"
+
+    _SWAP_MARKER: typing.Final[str] = "X"
+
+    def __init__(self, state: str) -> None:
+        self._state = state
+        self._blank_index = self._get_blank_index()
+
+    def can_move_to(self, action: Action) -> bool:
+        if action is Action.UP:
+            return self._get_blank_row() > 0
+
+        if action is Action.DOWN:
+            return self._get_blank_row() < (self.N_ROWS - 1)
+
+        if action is Action.LEFT:
+            return self._get_blank_col() > 0
+
+        if action is Action.RIGHT:
+            return self._get_blank_col() < (self.N_COLS - 1)
+
+        raise NotImplementedError(action)
+
+    def swap(self, action: Action) -> str | None:
+        if not self.can_move_to(action):
+            return None
+
+        if action is Action.UP:
+            target_number = self._state[self._blank_index - self.N_ROWS]
+        elif action is Action.DOWN:
+            target_number = self._state[self._blank_index + self.N_ROWS]
+        elif action is Action.LEFT:
+            target_number = self._state[self._blank_index - 1]
+        elif action is Action.RIGHT:
+            target_number = self._state[self._blank_index + 1]
+        else:
+            raise NotImplementedError(action)
+        return (
+            self._state.replace(target_number, self._SWAP_MARKER)
+            .replace(self.BLANK_CHAR, target_number)
+            .replace(self._SWAP_MARKER, self.BLANK_CHAR)
+        )
+
+    def _get_blank_index(self) -> int:
+        return self._state.index(self.BLANK_CHAR)
+
+    def _get_blank_row(self) -> int:
+        return self._blank_index // self.N_ROWS
+
+    def _get_blank_col(self) -> int:
+        return self._blank_index % self.N_COLS
 
 
 class Nodo:
@@ -26,8 +93,15 @@ def sucessor(estado: str) -> set[tuple[str, str]]:
     :param estado:
     :return:
     """
-    # substituir a linha abaixo pelo seu codigo
-    raise NotImplementedError
+    puzzle_state = PuzzleState(estado)
+    successors: set[tuple[str, str]] = set()
+
+    for action in Action:
+        next_state = puzzle_state.swap(action)
+        if next_state is None:
+            continue
+        successors.add((action.value, next_state))
+    return successors
 
 
 def expande(nodo: Nodo) -> set[Nodo]:
